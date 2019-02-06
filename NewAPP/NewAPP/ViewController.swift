8036f1363bc231a60f9aee908cba1902492c4bd2
brn.developers <brn.developers@achiever-121.local>
2019-02-06 14:50:22 +0530

    i am sending

---
+++ NewAPP/NewAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewAPP
+//
+//  Created by brn.developers on 06/02/19.
+//  Copyright © 2019 brn.developers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
